44ae6d09bc6e23d603369cfe9681b13ab2629eda
dreamleak <overhxul@gmail.com>
2021-11-01 18:07:49 -0400

    Create button wrapper and dynamically append clear button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+const buttonWrapper = document.querySelector('.button-wrapper');
 
 function createDivs(col, row) {
     for (let i = 0; i < (col * row); i++) {
@@ -28,3 +29,21 @@ function hoverGridSquares() {
 }
 
 hoverGridSquares();
+
+function clearCurrentGrid() {
+    // dynmatically create clear button.
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear';
+    clearButton.id = 'clear-button';
+    buttonWrapper.appendChild(clearButton);
+
+    const clickClearButton = document.getElementById('clear-button');
+        clickClearButton.addEventListener('click', () => {
+            console.log('be a [[BIG SHOT]] !!');
+            gridContainer.textContent = '';
+        });
+}
+
+clearCurrentGrid();
+
+// instead of prompt, add three grid size options for better UX and UI.
