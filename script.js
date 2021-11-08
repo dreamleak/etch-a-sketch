26f060317479fbfbbd6835d33c1c156f005f8392
dreamleak <overhxul@gmail.com>
2021-11-07 20:14:54 -0500

    Implement different colour options for button events then include default grid function

--- script.js
+++ script.js
@@ -14,21 +14,65 @@ function createDivs(col, row) {
 createDivs(16, 16);
 
 function hoverGridSquares() {
-    const gridSquare = document.querySelectorAll('.grid-squares');
 
     gridSquare.forEach(square => {
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = '#141414';
+            square.style.backgroundColor = 'hsl(0, 0%, 8%)';
         });
+    });
+}
 
-        // if mouseover is triggered, there must be mouseout.
-        // square.addEventListener('mouseout', () => {
-        //     square.style.backgroundColor = '#ffffff';
-        // });
+const gridSquare = document.querySelectorAll('.grid-squares');
+
+
+function changeSquaresToBlack() {
+    const getBlackButton = document.createElement('button');
+    getBlackButton.textContent = 'Black';
+    getBlackButton.id = 'black-button';
+    buttonWrapper.appendChild(getBlackButton);
+
+    const clickGetBlack = document.getElementById('black-button');
+        clickGetBlack.addEventListener('click', hoverGridSquares);
+}
+
+function changeSquaresToRGB() {
+    const getRandomColorButton = document.createElement('button');
+    getRandomColorButton.textContent = 'Rainbow';
+    getRandomColorButton.id = 'rgb-button';
+    buttonWrapper.appendChild(getRandomColorButton);
+
+    const clickGetRandomColor = document.getElementById('rgb-button');
+        clickGetRandomColor.addEventListener('click', () => {
+            gridSquare.forEach(square => {
+                square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'hsla(' + 
+                (Math.random() * 360) + ', 100%, 50%, 1)';
+            });
+        });
+    }); 
+}
+
+function changeSquaresToGrey() {
+    const getGreyButton = document.createElement('button');
+    getGreyButton.textContent = 'Greyscale';
+    getGreyButton.id = 'grey-button';
+    buttonWrapper.appendChild(getGreyButton);
+
+    const clickGetGrey = document.getElementById('grey-button');
+        clickGetGrey.addEventListener('click', () => {
+            gridSquare.forEach(square => {
+                square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'hsla(' + 
+                (Math.random() * 130) + ', 0%, 50% , 1)';
+             });
+        });
     });
 }
 
 hoverGridSquares();
+changeSquaresToBlack();
+changeSquaresToRGB();
+changeSquaresToGrey();
 
 function clearCurrentGrid() {
     // dynmatically create clear button.
@@ -38,25 +82,37 @@ function clearCurrentGrid() {
     buttonWrapper.appendChild(clearButton);
 
     const clickClearButton = document.getElementById('clear-button');
-        clickClearButton.addEventListener('click', () => {
-            gridContainer.textContent = '';
-        });
+    clickClearButton.addEventListener('click', () => {
+        gridContainer.textContent = '';
+    });
 }
 
 clearCurrentGrid();
 
 function resizeGridByTen() {
-    // instead of prompt, add grid resize button option for better UX and UI.
     const resizeByTen = document.createElement('button');
     resizeByTen.textContent = '10x10';
     resizeByTen.id = 'ten-by-ten-button';
     buttonWrapper.appendChild(resizeByTen);
 
     const clickResizeByTen = document.getElementById('ten-by-ten-button');
-        clickResizeByTen.addEventListener('click', () => {
-            createDivs(10, 10);
-        });
+    clickResizeByTen.addEventListener('click', () => {
+        createDivs(10, 10);
+    });
 }
 
 resizeGridByTen();
 
+function resizeBackToDefaultGrid() {
+    const defaultGridButton = document.createElement('button');
+    defaultGridButton.textContent = '16x16';
+    defaultGridButton.id = 'default-button';
+    buttonWrapper.appendChild(defaultGridButton);
+
+    const clickDefaultButton = document.getElementById('default-button');
+        clickDefaultButton.addEventListener('click', () => {
+            createDivs(16, 16);
+        });
+}
+
+resizeBackToDefaultGrid();
