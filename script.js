8b5c981ffe93b24558f6f74f1c3245513bb24ea6
dreamleak <overhxul@gmail.com>
2021-11-26 20:34:47 -0500

    Refactor UI: replace resize buttons with range slider

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 const buttonWrapper = document.querySelector('.button-wrapper');
+const gridWrapper = document.querySelector('.grid-wrapper');
+const outPutWrapper = document.querySelector('.output-value-wrapper');
 
 function createDivs(col, row) {
     for (let i = 0; i < (col * row); i++) {
@@ -11,10 +13,10 @@ function createDivs(col, row) {
     }
 }
 
-createDivs(16, 16);
+createDivs(30, 30);
 
 function hoverGridSquares() {
-
+    
     gridSquare.forEach(square => {
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'hsl(0, 0%, 8%)';
@@ -30,7 +32,7 @@ function changeSquaresToBlack() {
     getBlackButton.textContent = 'Black';
     getBlackButton.id = 'black-button';
     buttonWrapper.appendChild(getBlackButton);
-
+    
     const clickGetBlack = document.getElementById('black-button');
     clickGetBlack.addEventListener('click', hoverGridSquares);
 }
@@ -40,13 +42,13 @@ function changeSquaresToRGB() {
     getRandomColorButton.textContent = 'Rainbow';
     getRandomColorButton.id = 'rgb-button';
     buttonWrapper.appendChild(getRandomColorButton);
-
+    
     const clickGetRandomColor = document.getElementById('rgb-button');
     clickGetRandomColor.addEventListener('click', () => {
         gridSquare.forEach(square => {
             square.addEventListener('mouseover', () => {
                 square.style.backgroundColor = 'hsla(' +
-                    (Math.random() * 360) + ', 100%, 50%, 1)';
+                (Math.random() * 360) + ', 100%, 50%, 1)';
             });
         });
     });
@@ -57,8 +59,8 @@ function changeSquaresToGrey() {
     getGreyButton.textContent = 'Greyscale';
     getGreyButton.id = 'grey-button';
     buttonWrapper.appendChild(getGreyButton);
-
-
+    
+    
     const clickGetGrey = document.getElementById('grey-button');
     clickGetGrey.addEventListener('click', () => {
         gridSquare.forEach(square => {
@@ -87,41 +89,47 @@ function clearCurrentGrid() {
     clearButton.textContent = 'Clear';
     clearButton.id = 'clear-button';
     buttonWrapper.appendChild(clearButton);
-
+    
     const clickClearButton = document.getElementById('clear-button');
     clickClearButton.addEventListener('click', () => {
         gridSquare.forEach(square => {
             square.style.backgroundColor = null;
-        })
+        });
     });
 }
 
 clearCurrentGrid();
 
-function resizeGridByTen() {
-    const resizeByTen = document.createElement('button');
-    resizeByTen.textContent = '10x10';
-    resizeByTen.id = 'ten-by-ten-button';
-    buttonWrapper.appendChild(resizeByTen);
+// refactor resize buttons into one function for range slider.
+// use input instead of mousemove, since it's more UI-friendly.
 
-    const clickResizeByTen = document.getElementById('ten-by-ten-button');
-    clickResizeByTen.addEventListener('click', () => {
-        createDivs(10, 10);
-    });
+function gridResizeSlider() {
+    const rangeSlider = document.createElement('input');
+    rangeSlider.type = 'range';
+    rangeSlider.className = 'range-slider';
+    rangeSlider.min = '10';
+    rangeSlider.max = '30';
+    rangeSlider.step = '5';
+    gridWrapper.appendChild(rangeSlider);
+}
+ 
+gridResizeSlider();
+
+let showOutPut = document.querySelector('output'),
+    rangeTrack = document.querySelector('input');
+
+function outPutValue() {
+    showOutPut.innerHTML = rangeTrack.value;
+
+    rangeTrack.addEventListener('input', () => {
+        showOutPut.innerHTML = rangeTrack.value;
+    }, false);
+    outPutWrapper.appendChild(showOutPut);
 }
 
-resizeGridByTen();
+outPutValue();
 
-function resizeBackToDefaultGrid() {
-    const defaultGridButton = document.createElement('button');
-    defaultGridButton.textContent = '16x16';
-    defaultGridButton.id = 'default-button';
-    buttonWrapper.appendChild(defaultGridButton);
-
-    const clickDefaultButton = document.getElementById('default-button');
-    clickDefaultButton.addEventListener('click', () => {
-        createDivs(16, 16);
-    });
+function changeGridSize(){
+    createDivs(rangeTrack.value, rangeTrack.value);
 }
-
-resizeBackToDefaultGrid();
+rangeTrack.addEventListener('mouseup', changeGridSize);
